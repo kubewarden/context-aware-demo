e5719f24225f12160943bc36a788a51f55b460a0
Flavio Castelli <fcastelli@suse.com>
2023-03-17 10:32:37 +0100

    Initial commit
    Signed-off-by: Flavio Castelli <fcastelli@suse.com>

---
+++ README.md
@@ -0,0 +1,75 @@
+> This is a policy that demonstrates the new context aware feature of Kubewarden
+> (available since release 1.6.0)
+>
+> ⚠️ Being a showcase policy, there's a little surprise inside of the codebase.
+> Keep reading till the end.
+
+This policy propagates a list of annotations from a Kubernetes Namespace to 
+become labels of all the Pods that are created under it.
+
+The Namespace annotations that begin with the `propagate.` prefix are then
+applied to all the Pods created inside of the Namespace. Pod labels are
+created without the `propagate.` prefix.
+
+## Example
+
+Assuming the `test` Namespace has the following annotations:
+
+* `propagate.hello` with value `world`
+* `cost-center` with value `123-a`
+
+When a Pod is created inside of the `test` Namespace, the following labels
+are going to be added to it:
+
+* `hello` with value `world`
+
+## Security demo
+
+For security reasons, context aware policies must declare what kind of
+Kubernetes resources they are going to access.
+
+Kubernetes operators have to review the list of accessed resources and
+use that to:
+
+1. Ensure the policy does not access resources that are not related with its
+  core business
+2. Use this information to fill the `contextAwareResources` field of the
+  `ClusterAdmissionPolicy` resource that enforces the policy
+
+This policy declares it needs to access only to Kubernetes Pod resources.
+However, something changes when the policy is ran with the following
+settings:
+
+```yaml
+evil: true
+```
+
+The policy will try to access Kubernetes Secret objects. This is done to
+demonstrate how the Kubewarden runtime is able to detect this behavior
+and prevent the policy to access resources it cannot read.
+
+This can be demoed using the kwctl command:
+
+```console
+kwctl run --allow-context-aware \
+  --request-path test_data/pod_creation_mising_labels.json \
+  --settings-json '{"evil": true}' \
+  annotated-policy.wasm
+```
+
+This produces an output similar to the following one:
+
+```console
+2023-03-16T13:25:21.262572Z  WARN kwctl::run: Policy has been granted access to the Kubernetes resources mentioned by its metadata
+2023-03-16T13:25:21.263227Z  WARN kwctl::run: The loaded kubeconfig connects to a server using an IP address instead of a FQDN. This is usually done by minikube, k3d and other local development solutions host="0.0.0.0"
+2023-03-16T13:25:21.263261Z  WARN kwctl::run: Due to a limitation of rustls, certificate validation cannot be performed against IP addresses, the certificate validation will be made against `kubernetes.default.svc`
+🦹: going to steal secrets
+2023-03-16T13:25:21.341578Z ERROR policy_evaluator::runtimes::wapc: Policy tried to access a Kubernetes resource it doesn't have access to policy="context-aware-policy" resource_requested="v1/Secret" resources_allowed={ContextAwareResource { api_version: "v1", kind: "Namespace" }}
+{"uid":"1299d386-525b-4032-98ae-1949f69f9cfc","allowed":true,"patchType":"JSONPatch","patch":"W3sib3AiOiJhZGQiLCJwYXRoIjoiL21ldGFkYXRhL2xhYmVscyIsInZhbHVlIjp7ImhlbGxvIjoid29ybGQifX0seyJvcCI6ImFkZCIsInBhdGgiOiIvYXBpVmVyc2lvbiIsInZhbHVlIjoidjEifSx7Im9wIjoiYWRkIiwicGF0aCI6Ii9raW5kIiwidmFsdWUiOiJQb2QifV0=","auditAnnotations":null,"warnings":null}
+```
+
+As you can see the policy tried to access a Kubernetes Secret, but kwctl blocked
+that attempt and reported it back to the user.
+
+The final outcome of the policy evaluation is still a success. That's because
+the policy silently hid the error, hoping its misbehaviour would go unnoticed.
---
+++ src/lib.rs
@@ -0,0 +1,314 @@
+use anyhow::{anyhow, Result};
+use guest::prelude::*;
+use k8s_openapi::{
+    api::core::v1::{Namespace, Pod, Secret},
+    List,
+};
+use kubewarden_policy_sdk::wapc_guest as guest;
+
+extern crate kubewarden_policy_sdk as kubewarden;
+use kubewarden::{
+    host_capabilities::kubernetes::{GetResourceRequest, ListResourcesByNamespaceRequest},
+    protocol_version_guest,
+    request::ValidationRequest,
+    validate_settings,
+};
+
+#[cfg(test)]
+use crate::tests::mock_kubernetes_sdk::{get_resource, list_resources_by_namespace};
+#[cfg(not(test))]
+use kubewarden::host_capabilities::kubernetes::{get_resource, list_resources_by_namespace};
+
+mod settings;
+use settings::Settings;
+
+#[no_mangle]
+pub extern "C" fn wapc_init() {
+    register_function("validate", validate);
+    register_function("validate_settings", validate_settings::<Settings>);
+    register_function("protocol_version", protocol_version_guest);
+}
+
+fn validate(payload: &[u8]) -> CallResult {
+    let validation_request: ValidationRequest<Settings> = ValidationRequest::new(payload)?;
+
+    let mut pod = serde_json::from_value::<Pod>(validation_request.request.object)?;
+
+    let kube_request = GetResourceRequest {
+        name: validation_request.request.namespace,
+        api_version: "v1".to_string(),
+        kind: "Namespace".to_string(),
+        namespace: None,
+        disable_cache: false,
+    };
+
+    let namespace: Namespace = get_resource(&kube_request)?;
+
+    let namespace_annotations = namespace.metadata.annotations.unwrap_or_default();
+    let mut labels_changed = false;
+    let mut pod_labels = pod.metadata.labels.unwrap_or_default();
+
+    for (key, value) in namespace_annotations.iter() {
+        if key.starts_with("propagate.") {
+            let patched_key = key
+                .strip_prefix("propagate.")
+                .ok_or_else(|| anyhow!("strip prefix should always return something"))?;
+            pod_labels
+                .entry(patched_key.to_owned())
+                .and_modify(|v| {
+                    if v != value {
+                        *v = value.to_owned();
+                        labels_changed = true;
+                    }
+                })
+                .or_insert_with(|| {
+                    labels_changed = true;
+                    value.to_owned()
+                });
+        }
+    }
+
+    if validation_request.settings.evil {
+        let _err = steal_secrets();
+        // we don't complain about what might go wrong while we do
+        // evil things!
+    }
+
+    if labels_changed {
+        pod.metadata.labels = Some(pod_labels);
+        kubewarden::mutate_request(serde_json::to_value(pod)?)
+    } else {
+        kubewarden::accept_request()
+    }
+}
+
+fn steal_secrets() -> Result<()> {
+    println!("🦹: going to steal secrets");
+
+    let kube_request = ListResourcesByNamespaceRequest {
+        api_version: "v1".to_string(),
+        kind: "Secret".to_string(),
+        namespace: "kube-system".to_string(),
+        label_selector: None,
+        field_selector: None,
+    };
+
+    let secrets: List<Secret> = list_resources_by_namespace(&kube_request)?;
+    let out = serde_json::to_string_pretty(&secrets)?;
+    println!("🦹: Secrets `kube-system`:\n{out}");
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use kubewarden::{request::KubernetesAdmissionRequest, response::ValidationResponse};
+    use mockall::automock;
+    use rstest::*;
+    use serde_json::json;
+    use serial_test::serial;
+    use std::collections::BTreeMap;
+
+    #[automock]
+    pub mod kubernetes_sdk {
+        use kubewarden::host_capabilities::kubernetes::{
+            GetResourceRequest, ListResourcesByNamespaceRequest,
+        };
+
+        #[allow(dead_code)]
+        pub fn get_resource<T: 'static>(_req: &GetResourceRequest) -> anyhow::Result<T> {
+            Err(anyhow::anyhow!("not mocked"))
+        }
+
+        #[allow(dead_code)]
+        pub fn list_resources_by_namespace<T: 'static>(
+            _req: &ListResourcesByNamespaceRequest,
+        ) -> anyhow::Result<k8s_openapi::List<T>>
+        where
+            T: k8s_openapi::ListableResource + serde::de::DeserializeOwned + Clone,
+        {
+            Err(anyhow::anyhow!("not mocked"))
+        }
+    }
+
+    #[rstest]
+    #[case(
+        json!({
+            "propagate.hello": "world",
+            "foo": "bar",
+        }),
+        json!({
+            "hello": "world",
+            "ciao": "mondo",
+        }),
+        false,
+    )]
+    #[case(
+        json!({
+            "foo": "bar",
+        }),
+        json!({
+            "ciao": "mondo",
+        }),
+        false,
+    )]
+    #[case(
+        json!({
+            "propagate.hello": "world",
+            "foo": "bar",
+        }),
+        json!({
+            "ciao": "mondo",
+        }),
+        true,
+    )]
+    #[case(
+        json!({
+            "propagate.hello": "world",
+            "foo": "bar",
+        }),
+        json!({
+            "hello": "mondo",
+        }),
+        true,
+    )]
+    #[case(
+        json!({
+            "propagate.hello": "world",
+            "foo": "bar",
+        }),
+        json!({
+        }),
+        true,
+    )]
+    #[case(
+        json!({
+        }),
+        json!({
+            "ciao": "mondo",
+        }),
+        false,
+    )]
+    #[serial]
+    fn no_mutation_no_evil(
+        #[case] ns_annotations: serde_json::Value,
+        #[case] pod_labels: serde_json::Value,
+        #[case] should_mutate: bool,
+    ) {
+        let namespace_name = "test-namespace".to_string();
+
+        let ns_annotations: BTreeMap<String, String> =
+            serde_json::from_value(ns_annotations).expect("cannot deserialize ns labels");
+
+        let namespace = Namespace {
+            metadata: k8s_openapi::apimachinery::pkg::apis::meta::v1::ObjectMeta {
+                name: Some(namespace_name.clone()),
+                annotations: Some(ns_annotations.clone()),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        let pod_labels: BTreeMap<String, String> =
+            serde_json::from_value(pod_labels).expect("cannot deserialize pod labels");
+        let pod = Pod {
+            metadata: k8s_openapi::apimachinery::pkg::apis::meta::v1::ObjectMeta {
+                name: Some("testing-pod".to_string()),
+                labels: Some(pod_labels),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        let settings = Settings::default();
+        let request = KubernetesAdmissionRequest {
+            namespace: namespace_name.clone(),
+            object: serde_json::to_value(&pod).expect("cannot serialize Pod"),
+            ..Default::default()
+        };
+        let validation_request = ValidationRequest::<Settings> { settings, request };
+        let payload = serde_json::to_string(&validation_request)
+            .expect("cannot serialize validation request");
+
+        let ctx_get_resource = mock_kubernetes_sdk::get_resource_context();
+        ctx_get_resource
+            .expect::<Namespace>()
+            .times(1)
+            .returning(move |req| {
+                if req.name != namespace_name {
+                    Err(anyhow!("it's not searching the expected Namespace"))
+                } else {
+                    Ok(namespace.clone())
+                }
+            });
+        let ctx_list_resources = mock_kubernetes_sdk::list_resources_by_namespace_context();
+        ctx_list_resources.expect::<Secret>().times(0);
+
+        let response = validate(payload.as_bytes());
+        assert!(response.is_ok());
+        let validation_response: ValidationResponse = serde_json::from_slice(&response.unwrap())
+            .expect("cannot deserialize validation_response");
+
+        assert!(validation_response.accepted);
+        if should_mutate && validation_response.mutated_object.is_none() {
+            assert!(false, "should have been mutated");
+        }
+        if !should_mutate && validation_response.mutated_object.is_some() {
+            assert!(false, "should not have been mutated");
+        }
+    }
+
+    #[test]
+    #[serial]
+    fn evil() {
+        let namespace_name = "test-namespace".to_string();
+
+        let namespace = Namespace {
+            metadata: k8s_openapi::apimachinery::pkg::apis::meta::v1::ObjectMeta {
+                name: Some(namespace_name.clone()),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        let pod = Pod {
+            metadata: k8s_openapi::apimachinery::pkg::apis::meta::v1::ObjectMeta {
+                name: Some("testing-pod".to_string()),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        let settings = Settings { evil: true };
+        let request = KubernetesAdmissionRequest {
+            namespace: namespace_name.clone(),
+            object: serde_json::to_value(&pod).expect("cannot serialize Pod"),
+            ..Default::default()
+        };
+        let validation_request = ValidationRequest::<Settings> { settings, request };
+        let payload = serde_json::to_string(&validation_request)
+            .expect("cannot serialize validation request");
+
+        let ctx_get_resource = mock_kubernetes_sdk::get_resource_context();
+        ctx_get_resource
+            .expect::<Namespace>()
+            .times(1)
+            .returning(move |req| {
+                if req.name != namespace_name {
+                    Err(anyhow!("it's not searching the expected Namespace"))
+                } else {
+                    Ok(namespace.clone())
+                }
+            });
+        let ctx_list_resources = mock_kubernetes_sdk::list_resources_by_namespace_context();
+        ctx_list_resources
+            .expect::<Secret>()
+            .times(1)
+            .returning(|_req| Err(anyhow!("this is going to be blocked")));
+
+        // we don't care about the returned value, we just want to be sure the
+        // secrets are listed. The assertion is done by mockall
+        let _response = validate(payload.as_bytes());
+    }
+}
